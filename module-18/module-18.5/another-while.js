ff21d7dca6d591f28a725a6b0bcf35ed8c56d8e2
Zubaer Molla Arnab <89517348+ZubaerMolla@users.noreply.github.com>
2022-04-09 14:45:31 +0600

    Add files via upload

---
+++ module-18/module-18.5/another-while.js
@@ -0,0 +1,15 @@
+// odd number
+
+var number = 1; 
+while (number <= 20) {
+    console.log(number)
+    number  = number + 2;
+}
+
+// even number
+
+var number = 0; 
+while (number <= 20) {
+    console.log(number)
+    number  = number + 2;
+}
---
+++ module-18/module-18.5/for-loop.js
@@ -0,0 +1,5 @@
+console.log('running for loop')
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log('roast den please')
+    console.log(roastGiven);
+}
