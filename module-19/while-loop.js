1daac007e94447173ca24d59078db916fda50cb3
Zubaer Molla Arnab <89517348+ZubaerMolla@users.noreply.github.com>
2022-04-12 14:36:10 +0600

    Add files via upload

---
+++ module-19/while-loop.js
@@ -0,0 +1,16 @@
+var i = 0;
+while(i < 10){
+    console.log(i);
+    if(i == 8){
+        break;
+    }
+    i++;
+}
+
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+    if(i == 8){
+        break;
+    }
+}
